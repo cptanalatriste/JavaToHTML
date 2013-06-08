5f35e95578219209011edad3a31f8bfd93763de5
Carlos G. Gavidia <cgavidiac@gmail.com>
2013-06-08 13:02:20 -0500

    Improved sample program

--- sampleProgram.c
+++ sampleProgram.c
@@ -1,3 +1,16 @@
-int unaFuncion(int parametro) {
-} 
-unaFuncion(10)
+void configurar() {
+	definirTitulo(variableTitulo)
+	CajaTexto cajaTextoNombre
+	cajaTextoNombre = generarCajaTexto(variableNombreCaja, variableEsObligatorioCaja, 
+		variableEsAlfanumericoCaja, variableClaseCaja)
+
+	Boton botonEnviar
+	botonEnviar = generarBoton(variableNombreBoton, variableClaseCaja)
+
+	definirHojaDeEstilos(variableHojaEstilos)
+	definirPosicion(variablePosicion)
+	agregarElemento(cajaTextoNombre)
+	agregarElemento(botonEnviar)
+}
+generarFormulario()
+
